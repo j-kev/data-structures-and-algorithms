ae127b3b4363f65bcb441704a99bcd6efee2b9ff
Kevin Vergara <j.kevin@protonmail.com>
2022-11-23 16:34:16 -0500

    Add bubble sort algorithm

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(array):
+    length = len(array) - 1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for i in range(length):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                sorted = False
+        length -= 1
+
+    return array
+
+
+assert bubble_sort([5, 4, 3, 2, 0, 1]) == [0, 1, 2, 3, 4, 5]

30626f988144be3c90a5cb13a4786977bb7b404c
Kevin Vergara <j.kevin@protonmail.com>
2022-11-23 20:21:46 -0500

    Add selection sort algorithm

---
+++ selection_sort.py
@@ -0,0 +1,12 @@
+def selection_sort(array):
+    for i in range(len(array) - 1):
+        lowest = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[lowest]:
+                lowest = j
+        if lowest != i:
+            array[i], array[lowest] = array[lowest], array[i]
+    return array
+
+a = [9, 3, 6, 8, 5, 2, 0, 1, 7, 4]
+assert selection_sort(a) == [x for x in range(10)]
